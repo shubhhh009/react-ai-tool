f9ed0113b7fd2452f37884ea1823b5b743bc0d06
Shubham Tiwari <tiwarisachin8482@gmail.com>
2025-12-02 12:25:26 +0530

    Updated env and constants

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,2 @@
-export const  URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAFQHDaGoDeQC0XQreTyRqKY15vVwi-Uus"
- 
-  
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDkaRGyfiDSX1F6RG0NejhgQd80_jaIfLw"
+
